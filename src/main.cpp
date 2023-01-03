bf52075ca8945b7cc8b93c259f8dff32d815bd60
Johan Arcos (El Puma) <jarco0204@outlook.com>
2023-01-03 16:40:28 -0330

    simplest skeleton sketch

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+/*
+    Setup function
+*/
+void setup()
+{
+    // Serial Baud Rate
+    Serial.begin(115200);
+    Serial.println("Starting Setup...");
+}
+
+/*
+    loop function
+*/
+void loop()
+{
+}
